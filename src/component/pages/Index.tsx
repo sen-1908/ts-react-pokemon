ceb023aaf791b6804bce74665a5be53bf5676792
sen <sen121108@icloud.com>
2023-02-13 18:26:34 +0900

    notdoneImage

--- src/component/pages/Index.tsx
+++ src/component/pages/Index.tsx
@@ -1,9 +1,35 @@
-import { memo } from "react"
+import { memo, useEffect, useState } from "react"
+import axios from "axios";
 
+type pokemonData = {
+    name : string;
+    sprites : {
+        back_default : any;
+    } 
+} 
 export const Index: React.FC = memo(() => {
+    const [datas, setDatas] = useState([]);
+    
+    const wantNumber = 100;
+
+    const urlAPI = "https://pokeapi.co/api/v2/pokemon/25";
+    useEffect( () => {
+        axios.get(urlAPI).then((res)=> {
+        setDatas(res.data);
+        })
+    },[])
+    console.log(datas.sprites.back_default);
     return (
         <div>
-            <p>index</p>
+            <div>
+                {Object.values(datas).map((data: pokemonData,id: number) => (
+                <div key={id}  >
+                    <p>{data.name}</p>
+                    <img src={data.sprites.back_default} />  
+                    <p>{data.name}</p>
+                </div>
+                ))}
+            </div>
         </div>
     )
 
